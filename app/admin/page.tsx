31efd45cc41a20f2d081a3020788aeddfddd405e
AndresEGV <agomez.vg@gmail.com>
2025-01-22 10:41:38 -0300

    feat: editar presupuesots y cachear funciones

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,34 @@
+import getToken from "@/src/auth/token";
+import { BudgetsAPIResponseSchema } from "@/src/schemas";
+import { formatCurrency, formatDate } from "@/src/utils";
+import { Metadata } from "next";
+
 import Link from "next/link";
-import React from "react";
+import BudgetMenu from "../../components/budgets/BudgetMenu";
+
+export const metadata: Metadata = {
+  title: "CashTrackr - Panel de Administración",
+  description: "CashTrackr - Panel de Administración",
+};
+
+const getUserBudgets = async () => {
+  const token = getToken();
+  const url = `${process.env.API_URL}/budgets`;
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    next: {
+      tags: ["all-budggets"],
+    },
+  });
+  const json = await response.json();
+  const budgets = BudgetsAPIResponseSchema.parse(json);
+  return budgets;
+};
 
 const AdminPage = async () => {
+  const budgets = await getUserBudgets();
   return (
     <>
       <div className="flex flex-col-reverse md:flex-row md:justify-between items-center">
@@ -15,12 +42,57 @@ const AdminPage = async () => {
           </p>
         </div>
         <Link
-          href={"/admin/budget/new"}
+          href={"/admin/budgets/new"}
           className="bg-amber-500 p-2 rounded-lg text-white font-bold w-full md:w-auto text-center"
         >
           Crear Presupuesto
         </Link>
       </div>
+      {budgets.length ? (
+        <ul
+          role="list"
+          className="divide-y divide-gray-300 border shadow-lg mt-10 "
+        >
+          {budgets.map((budget) => (
+            <li key={budget.id} className="flex justify-between gap-x-6 p-5 ">
+              <div className="flex min-w-0 gap-x-4">
+                <div className="min-w-0 flex-auto space-y-2">
+                  <p className="text-sm font-semibold leading-6 text-gray-900">
+                    <Link
+                      href={`/admin/budgets/${budget.id}"}`}
+                      className="cursor-pointer hover:underline text-2xl font-bold"
+                    >
+                      {budget.name}
+                    </Link>
+                  </p>
+                  <p className="text-xl font-bold text-amber-500">
+                    {formatCurrency(+budget.amount)}
+                  </p>
+                  <p className="text-gray-500  text-sm">
+                    última actualización:{" "}
+                    <span className="font-bold">
+                      {formatDate(budget.updatedAt)}
+                    </span>
+                  </p>
+                </div>
+              </div>
+              <div className="flex shrink-0 items-center gap-x-6">
+                <BudgetMenu budgetId={budget.id} />
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-center py-20">
+          No hay presupuestos aún {""}
+          <Link
+            href={"/admin/budgets/new"}
+            className="text-purple-950 font-bold"
+          >
+            comienza Creando uno
+          </Link>
+        </p>
+      )}
     </>
   );
 };
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -47,6 +47,26 @@ export const ResetPasswordSchema = z
     path: ["password_confirmation"],
   });
 
+export const DraftBudgetSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: "El Nombre del presupuesto es obligatorio" }),
+  amount: z.coerce
+    .number({ message: "Cantidad no válida" })
+    .min(1, { message: "Cantidad no válida" }),
+});
+
+export const BudgetAPIResponseSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  amount: z.string(),
+  userId: z.number(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+});
+
+export const BudgetsAPIResponseSchema = z.array(BudgetAPIResponseSchema);
+
 export const SuccessSchema = z.string();
 export const ErrorResponseSchema = z.object({
   error: z.string(),
@@ -59,3 +79,4 @@ export const UserSchema = z.object({
 });
 
 export type User = z.infer<typeof UserSchema>;
+export type Budget = z.infer<typeof BudgetAPIResponseSchema>;
